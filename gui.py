16471052562dc61d88bfb0f6840cd5f3b404f1f3
QuantumY <45296740+QuantumY@users.noreply.github.com>
2021-11-07 13:46:55 -0800

    improved GUI (not working)

--- gui.py
+++ gui.py
@@ -5,36 +5,33 @@ from kivy.uix.button import Button
 from kivy.graphics import Color, Ellipse, Line
 from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 class TokenInput(TextInput):
-    def build(self):
-        self.multiline = False
-        self.text = "cryptocurrency"
-        self.focus = True
-        self.bind(on_text_validation=on_enter)
-        self.bind(text=on_text)
-    
     def insert_text(self, substring, from_undo=False):
         s = substring.upper()
         print(s)
         return super(TokenInput, self).insert_text(s, from_undo=from_undo)
 
-    def on_enter(self, value):
-        print('User pressed enter in: ', self)
-
+    '''
     def on_text(self, value, from_undo=False):
         print('Cryptocurrency selected: ' + str(value))
+    '''
 
-class MyApp(App):
+    def on_enter(self, value):
+        print('Cryptocurrency selected: ' + str(value))
+
+class SelectionScreen(Screen):
+    pass
+
+class DataWatchApp(App):
     def build(self):
-        parent = Widget()
+        screen_manager = ScreenManager()
+        selection_screen = SelectionScreen()
+        screen_manager.add_widget(selection_screen)
         Window.clearcolor = (255, 255, 255, 0) # background color for the window
-        '''tokeninput = TextInput(multiline=False, text='cryptocurrency', focus=True)
-        parent.add_widget(tokeninput)
-        tokeninput.bind(on_text_validation=on_enter)
-        tokeninput.bind(text=on_text)'''
-        tokeninput = TokenInput()
-        parent.add_widget(tokeninput)
-        return parent
 
-MyApp().run()
+        return screen_manager
+
+DataWatchApp().run()
